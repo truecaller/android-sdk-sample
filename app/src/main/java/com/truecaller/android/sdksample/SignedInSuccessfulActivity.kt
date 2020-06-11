03a6ba05563884fea2cd8efef865916b0e054131
shubhral <shubhral.kumar@truecaller.com>
2020-06-11 17:16:13 +0530

    layout & background refactored for different flows

--- app/src/main/java/com/truecaller/android/sdksample/SignedInSuccessfulActivity.kt
+++ app/src/main/java/com/truecaller/android/sdksample/SignedInSuccessfulActivity.kt
@@ -1,11 +1,9 @@
 package com.truecaller.android.sdksample
 
 import android.os.Bundle
-import android.view.View
 import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
-import kotlinx.android.synthetic.main.activity_signed_in_successful.successImage
 import kotlinx.android.synthetic.main.activity_signed_in_successful.successText
 import kotlinx.android.synthetic.main.activity_signed_in_successful.welcomeText
 
@@ -19,13 +17,12 @@ class SignedInSuccessfulActivity : AppCompatActivity() {
         val name = intent.getStringExtra("name")
         name?.let { welcomeText.text = String.format("Welcome, %s!", it) }
         when (flowType) {
-            FLOW1 -> rootLayout.setBackgroundColor(ContextCompat.getColor(this, R.color.flow1))
-            FLOW2 -> rootLayout.setBackgroundColor(ContextCompat.getColor(this, R.color.flow2))
+            FLOW1 -> rootLayout.background = ContextCompat.getDrawable(this, R.drawable.success_bg_layer_flow1)
+            FLOW2 -> rootLayout.background = ContextCompat.getDrawable(this, R.drawable.success_bg_layer_flow2)
             FLOW3, FLOW4 -> {
                 rootLayout.background = ContextCompat.getDrawable(this, R.drawable.success_bg_layer)
                 welcomeText.setTextColor(ContextCompat.getColor(this, android.R.color.black))
                 successText.setTextColor(ContextCompat.getColor(this, android.R.color.black))
-                successImage.visibility = View.INVISIBLE
             }
         }
     }
